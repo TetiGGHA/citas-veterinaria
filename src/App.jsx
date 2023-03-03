9c9a78d3fd8de6f47781c2a09221d311e15e243e
Gilberto Hernandez <tetiggha@gmail.com>
2023-03-03 16:45:38 -0300

    Instalacion de tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/Header.jsx";
+import Formulario from "./components/Formulario.jsx";
+import ListadoPacientes from "./components/ListadoPacientes.jsx";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <Formulario />
+      <ListadoPacientes />
+    </div>
+  );
 }
 
 export default App;
